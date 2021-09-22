b80014553e54b0f208997357da1663fc7e1f00a2
Brian Weedon <brian.weedon@proactis.com>
2021-09-22 17:17:50 -0400

    Added tab config for 4 spaces

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,12 @@
 -- This doesn't work in the keymappings section, need to figure out why
 vim.g.mapleader = " " 
 vim.g.maplocalleader = " "
+
+-- TODO: Move the tab stuff to another file later
+vim.opt.expandtab = true  -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4    -- Size of indent
+vim.opt.tabstop = 4       -- Number of spaces tabs count for
+
 -- Load the plugins
 require('plug')
 -- Load the plugin configurations
